b62d92311294c92c8b422913ffeb8ad3a8baaf91
cventura <cventura@outlook.com>
2015-03-27 13:16:00 -0700

    ...

---
+++ PreFundacion/Controllers/TutorsController.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using PreFundacion.Models;
+
+namespace PreFundacion.Controllers
+{
+    public class TutorsController : Controller
+    {
+        private PreFundacionEntities db = new PreFundacionEntities();
+
+        // GET: Tutors
+        public ActionResult Index()
+        {
+            return View(db.Tutors.ToList());
+        }
+
+        // GET: Tutors/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tutor tutor = db.Tutors.Find(id);
+            if (tutor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tutor);
+        }
+
+        // GET: Tutors/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Tutors/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "IDTutor,CURP,Nombre,ApellidoPaterno,ApellidoMaterno,Actividad,Domicilio")] Tutor tutor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Tutors.Add(tutor);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(tutor);
+        }
+
+        // GET: Tutors/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tutor tutor = db.Tutors.Find(id);
+            if (tutor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tutor);
+        }
+
+        // POST: Tutors/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "IDTutor,CURP,Nombre,ApellidoPaterno,ApellidoMaterno,Actividad,Domicilio")] Tutor tutor)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(tutor).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(tutor);
+        }
+
+        // GET: Tutors/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Tutor tutor = db.Tutors.Find(id);
+            if (tutor == null)
+            {
+                return HttpNotFound();
+            }
+            return View(tutor);
+        }
+
+        // POST: Tutors/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Tutor tutor = db.Tutors.Find(id);
+            db.Tutors.Remove(tutor);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
